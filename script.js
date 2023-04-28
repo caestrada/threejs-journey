2d3500d5c799c79e5a3ccffad245fbaf02fa8ea8
Carlos Estrada <cestr008@gmail.com>
2023-04-28 09:10:23 -0400

    My first cube

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log(THREE);
+/**
+ * 4 elements to get started:
+ * - A scene that will contain objects
+ * - Some objects
+ * - A camera
+ * - A renderer
+ */
+
+/** 
+ * Scene
+ * - Like a container
+ * - We put objects, models, light, etc..
+ * - At some point we ask Three.js to render the scene.
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ * -------
+ * - Primitive geometries
+ * - Imported models
+ * - Particles
+ * - Lights
+ * - Etc..
+ * 
+ * To create a visible obj we start with a Mesh.
+ */
+
+// Red cube
+const geometry = new THREE.BoxGeometry( 1, 1, 1 ) // Note: Unit 1. It could be any unit.
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } )
+const mesh = new THREE.Mesh( geometry, material )
+scene.add( mesh );
+
+// Camera
+const sizes = {
+    width: 800,
+    height: 600
+}
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 1, 1000 )
+camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
+
+
+scene.add( camera );
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
