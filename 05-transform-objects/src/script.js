0ecbb88105810084f8ba455e579ace1c412c6b93
Carlos Estrada <cestr008@gmail.com>
2023-04-30 18:38:30 -0400

    Transform a group of objects

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,12 +9,38 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
 
 // Position
-mesh.position.set(0.7, -0.6, 0)
+// mesh.position.set(0.7, -0.6, 0)
 // OR
 // mesh.position.x = 0.7
 // mesh.position.x = -0.6
@@ -26,14 +52,14 @@ mesh.position.set(0.7, -0.6, 0)
 // mesh.scale.y = 0.5
 // mesh.scale.z = 0.5
 // OR
-mesh.scale.set(2, 0.5, 0.5)
+// mesh.scale.set(2, 0.5, 0.5)
 
 
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI*0.25
-mesh.rotation.y = Math.PI*0.25
-scene.add(mesh)
+// // Rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI*0.25
+// mesh.rotation.y = Math.PI*0.25
+// scene.add(mesh)
 
 
 // Axes helper
@@ -58,7 +84,7 @@ camera.position.y = 1
 camera.position.x = 1
 scene.add(camera)
 
-camera.lookAt(mesh.position)
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
