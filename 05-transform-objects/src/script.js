88908f5d53805e889ea37ffc82d0f8da80eadcb7
Carlos Estrada <cestr008@gmail.com>
2023-04-30 18:29:32 -0400

    Manipulate object in space

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -12,7 +12,34 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+// Position
+mesh.position.set(0.7, -0.6, 0)
+// OR
+// mesh.position.x = 0.7
+// mesh.position.x = -0.6
+// mesh.position.x = 1
+
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// OR
+mesh.scale.set(2, 0.5, 0.5)
+
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI*0.25
+mesh.rotation.y = Math.PI*0.25
 scene.add(mesh)
+
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper)
+
 
 /**
  * Sizes
@@ -27,7 +54,11 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
+
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
